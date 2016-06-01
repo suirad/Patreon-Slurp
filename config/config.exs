1e9aeaf862274021e383cb799f19473acd1e97de
Suirad <suirad@users.noreply.github.com>
2016-06-01 18:29:31 -0500

    Config template

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,8 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :patreon_slurp, id: ""
+config :patreon_slurp, secret: ""
+config :patreon_slurp, token: ""
+config :patreon_slurp, refresh: ""
